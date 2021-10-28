9e4bdb5835dea9e5f9be124494a10975c1eb816a
Helen Aquino <helen.aquino@shaw.ca>
2021-10-28 01:11:19 -0600

    basic login page

--- src/Pages/Login/login.js
+++ src/Pages/Login/login.js
@@ -1,5 +1,75 @@
+import 'antd/dist/antd.css';
+import './login.css';
+import { Form, Input, Button, Checkbox } from 'antd';
+
 const Login = () => {
-	return <div>Login Page</div>;
+	// return <div>Login Page</div>;
+	// code from Ant Design Documentation: https://ant.design/components/form/
+	return (
+		<Form
+      name="basic"
+      labelCol={{
+        span: 8,
+      }}
+      wrapperCol={{
+        span: 16,
+      }}
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your username!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!',
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        name="remember"
+        valuePropName="checked"
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
+
+      <Form.Item
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+	);
 };
 
 export default Login;
