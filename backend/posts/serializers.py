db41b671fbdd1c5a69420428a034d0cbafac61ba
Aniket Mishra <ianiket23@gmail.com>
2021-10-27 21:24:31 -0600

    update

---
+++ backend/posts/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import posts
+
+class PostsSerializer(serializers.ModelSerializer):
+
+    type = serializers.SerializerMethodField()
+
+    host = serializers.SerializerMethodField()
+
+    url = serializers.SerializerMethodField()
+
+    class Meta:
+        model = posts
+        fields = ("type", "post_id", "author_id", "title", "visibility")
+
+    def get_type(self, obj):
+        return "post"
+ 
--- backend/posts/views.py
+++ backend/posts/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
+from drf_yasg.utils import swagger_auto_schema
+
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework.decorators import (
+    api_view,
+    permission_classes,
+)
+from rest_framework import status
+from rest_framework.permissions import (
+    IsAuthenticatedOrReadOnly,
+)
+
+from .models import posts
+from .serializers import *
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
+@api_view(["GET"],["POST"])
+def getAllPosts(request: Request, id):
+    """
+    GET to get a list of all authors, with pagination options
+    POST to register a new author
+    """
+    if request.method == "GET":
+        try:
+            
+            post = posts.objects.filter(=id)
+            s = FollowerSerializer(author, context={'request': request}, many=True)
+            return Response(s.data)
+        except Author.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
