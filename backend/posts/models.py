06f6204c1a512df918a73d32236669b76a6cfee8
Aniket Mishra <ianiket23@gmail.com>
2021-10-27 18:55:46 -0600

    Update models.py

--- backend/posts/models.py
+++ backend/posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+import uuid
+from backend import author
 # Create your models here.
+class posts(models.Model):
+    visibility_choice = (("PUBLIC", "PRIVATE"))
+    post_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    author_id = models.ForeignKey(author)
+    title = models.CharField("title", max_length= 100, unique= False, null = False, blank  = False)
+    visibility = models.CharField(choices=visibility_choice, blank = False, default = "PUBLIC")
+    
