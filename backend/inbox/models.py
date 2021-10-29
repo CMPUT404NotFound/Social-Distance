f203cb54dc9d1b738b5b395a668e28fa7c1908a9
Goldentoaste <rayg2375@gmail.com>
2021-10-29 13:02:15 -0700

    stuff

--- backend/inbox/models.py
+++ backend/inbox/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+from author.models import Author
+
+class InboxItem(models.Model):
+    
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    
+    # post = models
+    
