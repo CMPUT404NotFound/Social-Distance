a58dd484336ea74e95351311d86db16ca310994f
Nadeen <nadeen@ualberta.ca>
2021-10-29 15:47:45 -0600

    reset logged in initial state

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -14,7 +14,7 @@ import Explore from "./Pages/Explore/explore";
 import Error404 from "./Error/error404";
 
 const Routes = () => {
-	const [loggedIn, setLoggedIn] = useState(true);
+	const [loggedIn, setLoggedIn] = useState(false);
 	const [user, setUser] = useState(null);
 
 	if (loggedIn) {
