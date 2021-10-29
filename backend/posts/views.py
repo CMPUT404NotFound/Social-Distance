1655f093a77095c202188c8d37180de850de4a06
Aniket Mishra <ianiket23@gmail.com>
2021-10-29 04:26:19 -0600

    pagination

--- backend/posts/views.py
+++ backend/posts/views.py
@@ -20,17 +20,38 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 @swagger_auto_schema(method="post",tags=['posts'])
 @api_view(["GET","POST"])
 def getAllPosts(request: Request, author_id):
+    # if request.method == "GET":
+    #     try:
+    #         #pagination
+    #         post = posts.objects.filter(author_id=author_id)
+    #         s = PostsSerializer(post, context={"request": request}, many=True)
+    #         return Response(s.data)
+            
+    #     except posts.DoesNotExist:
+    #         return Response(status=status.HTTP_404_NOT_FOUND)
+      
     if request.method == "GET":
         try:
+            params: dict = request.query_params
+
             post = posts.objects.filter(author_id=author_id)
-            s = PostsSerializer(post, context={"request": request}, many=True)
-            return Response(s.data)
-            
+            if (
+                "page" in params and "size" in params
+            ):  # make sure param has both page and size in order to paginate
+                try:
+                    paginator = Paginator(
+                        post, int(params["size"]), allow_empty_first_page=True
+                    )  # create paginator with size
+                    s = PostsSerializer(
+                        paginator.page(int(params["page"])), many=True
+                    )  # get requested page and serialize
+                except (ValueError, EmptyPage, PageNotAnInteger) as e:
+                    return Response(str(e), status=status.HTTP_404_NOT_FOUND)
+            else:
+                s = PostsSerializer(post, context={"request": request}, many=True)
+            return Response(s.data)  
         except posts.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
-      
-        
-        
     elif request.method == "POST":
         try:
             author = Author.objects.get(pk = author_id)
