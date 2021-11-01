b97800aa9dfea38a406d964bc12ce71a02f5872b
Phousanak Han <phousana@ualberta.ca>
2021-11-01 16:01:20 -0600

    check for duplicate followers & more followers test

--- backend/Followers/tests.py
+++ backend/Followers/tests.py
@@ -1,6 +1,10 @@
+from logging import exception
 from django.http import response
+from django.http import request
+from django.http.request import HttpRequest
 from django.test import TestCase
 from django.contrib.auth.models import User
+from rest_framework import views
 from author.models import *
 from Followers.models import *
 
@@ -21,8 +25,8 @@ class FollowerTestCase(TestCase):
                                        "test_user2@github", "", "test_password2")
         self.usr3 = self.create_author("test_user3", "test_user3",
                                        "test_user3@github", "", "test_password3")
-        #self.create_author("test_user2", "test_password2")
-        #self.create_author("test_user3", "test_password3")
+        # self.create_author("test_user2", "test_password2")
+        # self.create_author("test_user3", "test_password3")
 
     def create_author(self, username, display_name, github, profile_image, password):
         author = Author.objects.create_superuser(userName=username, displayName=display_name, github=github,
@@ -31,18 +35,48 @@ class FollowerTestCase(TestCase):
         return author
 
     def test_get_authors(self):
-        #Follower.objects.create(sender=self.usr1, receiver=self.usr2)
+        # Follower.objects.create(sender=self.usr1, receiver=self.usr2)
         response = self.client.get("/api/authors")
         assert response.status_code == 200
 
     # not working; something is up with the id
-    # def test_get_followers(self):
-    #     Follower.objects.create(sender=self.usr1, receiver=self.usr2)
-    #     response = self.client.get("/api/author/{self.usr2.id}/followers")
-    #     assert response.status_code == 200
+    def test_get_followers_by_id(self):
+        Follower.objects.create(sender=self.usr1, receiver=self.usr2)
+        str_id2 = str(self.usr2.id)
+        response = self.client.get(f"/api/author/{str_id2}/followers")
+        # print("RESPONSE: ", response.json()[0]['id'])
+        assert response.status_code == 200
 
-    # def test_get_followers_individual(self):
-    #     Follower.objects.create(sender=self.usr1, receiver=self.usr2)
-    #     response = self.client.get(
-    #         "/api/author/{self.usr2.id}/followers/{self.usr1.id}")
-    #     assert response.status_code == 200
+    def test_get_single_follower_by_id(self):
+        Follower.objects.create(sender=self.usr1, receiver=self.usr2)
+        str_id1 = str(self.usr1.id)
+        str_id2 = str(self.usr2.id)
+        response = self.client.get(
+            f"/api/author/{str_id2}/followers/{str_id1}")
+        assert response.status_code == 200
+
+    def test_delete_follower_of_id(self):
+        Follower.objects.create(sender=self.usr1, receiver=self.usr2)
+        str_id1 = str(self.usr1.id)
+        str_id2 = str(self.usr2.id)
+        response = self.client.delete(
+            f"/api/author/{str_id2}/followers/{str_id1}")
+        assert response.status_code == 200
+
+    def test_follower_duplication(self):
+        Follower.objects.create(sender=self.usr1, receiver=self.usr2)
+        str_id1 = str(self.usr1.id)
+        str_id2 = str(self.usr2.id)
+        response = self.client.put(
+            f"/api/author/{str_id2}/followers/{str_id1}")
+        response2 = self.client.put(
+            f"/api/author/{str_id2}/followers/{str_id1}")
+        assert response2.status_code == 400
+
+    def test_follower_not_exist(self):
+        Follower.objects.create(sender=self.usr1, receiver=self.usr2)
+        str_id1 = str(self.usr1.id)
+        str_id3 = str(self.usr3.id)
+        response = self.client.get(
+            f"/api/author/{str_id3}/followers/{str_id1}")
+        assert response.status_code == 404
