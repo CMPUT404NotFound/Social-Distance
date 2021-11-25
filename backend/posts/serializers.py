ad5e3413a59711f2792d53febb47e3623a7fdf7d
Aniket Mishra <ianiket23@gmail.com>
2021-11-25 15:44:37 -0700

    migration mayhem. Save me PLease

--- backend/posts/serializers.py
+++ backend/posts/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
 from .models import content_choice, visibility_choice, Post
 from backend.settings import SITE_ADDRESS
 from author.serializers import AuthorSerializer 
@@ -13,12 +14,14 @@ class PostsSerializer(serializers.ModelSerializer):
     origin = serializers.SerializerMethodField('get_origin_id')
     contentType = ChoiceField(choices=content_choice)
     visibility = ChoiceField(choices=visibility_choice)
+    comments = SerializerMethodField()
 
     class Meta:
         model = Post
         fields = ("type", "post_id", "author", "title", 
         "visibility","description","content", "contentType",
-        "source", "origin","count","categories","comments","unlisted","published")
+        "source", "origin","count","categories","comments",
+        "unlisted","published")
 
     def get_type(self, obj):
         return "post"
@@ -36,3 +39,5 @@ class PostsSerializer(serializers.ModelSerializer):
             return obj.source
         else:
             return f"{SITE_ADDRESS}/author/{obj.author.id}/posts/{obj.post.id}"
+    def get_comments(self,obj):
+        return f"{SITE_ADDRESS}/author/{obj.author.id}/posts/{obj.post.id}/comments"
