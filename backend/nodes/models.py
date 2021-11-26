f01da802e154112ed07f74b04c917fcc45400d39
Goldentoaste <rayg2375@gmail.com>
2021-11-26 03:49:57 -0800

    bruh

--- backend/nodes/models.py
+++ backend/nodes/models.py
@@ -2,14 +2,18 @@ from django.db import models
 
 # Create your models here.
 
-
+class NodeManager(models.Manager):
+    
+    def create(self, *args, **kwargs):
+        
+        node :Node =  super().create(*args, **kwargs)
+        node.netloc = ""
 
 
 class Node(models.Model):
     
     url = models.URLField('url of node', unique=True, null=False, blank=False)
-    allowIncoming = models.BooleanField()
-    allowOutgoing = models.BooleanField()
+    netloc = models.CharField('netloc of node', max_length=100,  null=False, blank=False,default="")
     
     authRequiredIncoming = models.BooleanField()
     authRequiredOutgoing = models.BooleanField()
