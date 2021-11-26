d38355697b17abee651051faa7e7ba2edba74fa4
Phousanak Han <phousana@ualberta.ca>
2021-11-25 19:25:00 -0700

    globalsetting merge

--- backend/globalSetting/admin.py
+++ backend/globalSetting/admin.py
@@ -1,26 +1,26 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# from globalSetting.models import Setting
+from globalSetting.models import Setting
 
-# # Register your models here.
-
-
-# class GlobalSettingAdmin(admin.ModelAdmin):
-
-#     fields = ('newUserRequireActivation',)
-
-#     def __init__(self, model, admin_site) -> None:
-#         super().__init__(model, admin_site)
-#         if not Setting.objects.all().exists():
-#             print('adding 1st setting')
-#             Setting.objects.create()
+# Register your models here.
 
 
-#     def has_add_permission(self, request) -> bool:
-#         return False
+class GlobalSettingAdmin(admin.ModelAdmin):
 
-#     def has_delete_permission(self, request , obj = False) -> bool:
-#         return False
+    fields = ('newUserRequireActivation',)
+
+    def get_list_display(self, request):
+
+        if not Setting.objects.all().exists():
+            print('adding 1st setting')
+            Setting.objects.create()
+        return super().get_list_display(request)
+
+    def has_add_permission(self, request) -> bool:
+        return False
+
+    def has_delete_permission(self, request, obj=False) -> bool:
+        return False
 
 
-# admin.site.register(Setting, GlobalSettingAdmin)
+admin.site.register(Setting, GlobalSettingAdmin)
