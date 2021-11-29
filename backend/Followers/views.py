f00b2ac11330697370ef21b34daa7814092235d6
Phousanak Han <phousana@ualberta.ca>
2021-11-29 01:09:31 -0700

    getAllFollowers in progress, need testing

--- backend/Followers/views.py
+++ backend/Followers/views.py
@@ -26,6 +26,8 @@ from .models import *
 from .serializers import *
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
+from utils.request import checkIsLocal, ClassType, makeRequest
+import json
 
 
 @swagger_auto_schema(method="get", tags=['followers'])
@@ -34,10 +36,40 @@ def getAllFollowers(request: Request, id):
     if request.method == "GET":
         try:
             #author = Author.objects.get(pk=id)
-            author = Author.objects.filter(sender__receiver=id)
-            s = FollowerSerializer(
-                author, context={'request': request}, many=True)
-            return Response(s.data)
+            #author = Author.objects.filter(sender__receiver=id)
+            results = []
+            # followers = Follower.objects.filter(receiver = id)
+            follower_object = Follower.objects.filter(receiver = id)
+            #print("follower_object: ", follower_object)
+            for follower in follower_object:
+                follower_id = follower.sender
+                print("follower_id:", follower_id.split('id')[1][2: ]) #follower_id:  ['author: Bob, ', ': 6098687c-11f9-43ab-afa0-484a18b72356']
+                just_id = follower_id.split('id')[1][2:]
+                print("TYPE: ", type(follower_id))
+                object = checkIsLocal(just_id, ClassType.author)
+                print("HERE checkIsLocal(follower_id): ", object)
+                if not object.isLocal:
+                    response = makeRequest("GET", object.longId)
+                    print("HERE response: ", response)
+                    if 100 < response[1] < 300:
+                        results.append(json.loads(response[0]))
+                    
+
+
+
+
+
+                results.append(AuthorSerializer(object).data)
+            
+
+            s = {
+                'type': "followers",
+                'items': results
+            }
+            return Response(s)
+            # s = FollowerSerializer(
+            #     author, context={'request': request}, many=True)
+            # return Response(s.data)
         except Author.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
 
@@ -54,12 +86,12 @@ def addFollower(request: Request, author_id, follower_id):
             sender = Author.objects.get(pk=follower_id)
             try:
                 follower_exist = Follower.objects.get(
-                    sender=sender, receiver=receiver)
+                    sender=follower_id, receiver=author_id)
                 if follower_exist:
                     return Response(status=status.HTTP_400_BAD_REQUEST)
             except:
                 pass
-            follow = Follower.objects.create(sender=sender, receiver=receiver)
+            follow = Follower.objects.create(sender=follower_id, receiver=follower_id)
             # author.sender.add(follower)
             follow.save()
             return Response(status=status.HTTP_201_CREATED)
@@ -70,7 +102,7 @@ def addFollower(request: Request, author_id, follower_id):
         try:
             author = Author.objects.get(pk=author_id)
             follower = Author.objects.get(pk=follower_id)
-            follow = Follower.objects.get(sender=follower, receiver=author)
+            follow = Follower.objects.get(sender=follower_id, receiver=follower_id)
             follow.delete()
             return Response(status=status.HTTP_200_OK)
         except Author.DoesNotExist:
@@ -80,7 +112,7 @@ def addFollower(request: Request, author_id, follower_id):
         try:
             author = Author.objects.get(pk=author_id)
             follower = Author.objects.get(pk=follower_id)
-            follow = Follower.objects.get(sender=follower, receiver=author)
+            follow = Follower.objects.get(sender=follower_id, receiver=follower_id)
             if follow:
                 return Response(status=status.HTTP_200_OK)
         except Follower.DoesNotExist:
